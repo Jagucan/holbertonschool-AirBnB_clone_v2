5f9a15584d7503e3c39bec7c6d23ae8be0fcf1d3
Dannyelgeek <daot1993@gmail.com>
2023-03-21 10:55:49 -0500

    console AirBnB clone V2 update.

--- console.py
+++ console.py
@@ -113,37 +113,32 @@ class HBNBCommand(cmd.Cmd):
         """ Overrides the emptyline method of CMD """
         pass
 
-    def do_create(self, args):
+    def do_create(self, arg):
         """ Create an object of any class"""
         if not args:
             print("** class name missing **")
             return
         
-        args = args.split(" ")
-        class_name = args[0]
-
-        if class_name not in HBNBCommand.classes:
+        args = arg.split()
+        if args[0] not in self.classes:
             print("** class doesn't exist **")
             return
-        
-        attr = {}
+
+        obj = eval(args[0])()
         for param in args[1:]:
-            new_dict = param.split(" ", 1)
-            attr[new_dict[0]] = new_dict[1]
-        
-        new_instance = HBNBCommand.classes[class_name]()
-
-        for key, val in attr.items():
-            if val.startswith('"'):
-                val = val[1:-1].replace("_", " ").replace('\\"', '"')
-            elif "." in val:
-                val = float(val)
-            else:
-                val = int(val)
-            setattr(new_instance, key, val)
-
-        print(new_instance.id)
-        new_instance.save()
+            try:
+                key, value = param.split("=", 1)
+                if value.startswith('"'):
+                    value = value[1:-1].replace("_", " ").replace('\\"', '"')
+                elif "." in value:
+                    value = float(value)
+                else:
+                    value = int(value)
+                setattr(obj, key, value)
+            except Exception:
+                pass
+        obj.save()
+        print(obj.id)
 
     def help_create(self):
         """ Help information for the create method """
