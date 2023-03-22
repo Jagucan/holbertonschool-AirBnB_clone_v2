a9038edf8d1c7388baba911c1f1aa55f69676d56
Dannyelgeek <daot1993@gmail.com>
2023-03-22 12:48:39 -0500

    base_model AirBnB clone V2 update.

--- models/base_model.py
+++ models/base_model.py
@@ -18,18 +18,17 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Instantiates a new model"""
         from models import storage
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
-        else:
+        self.id = kwargs.get("id", str(uuid.uuid4()))
+        self.created_at = kwargs.get("id", datetime.now())
+        self.updated_at = kwargs.get("id", datetime.now())
+        if kwargs:
             for key, value in kwargs.items():
                 if key != '__class__':
                     if key in ('created_at', 'updated_at'):
                         value = datetime.strptime(
                             value, '%Y-%m-%dT%H:%M:%S.%f')
                     setattr(self, key, value)
+        storage.new(self)
 
     def __str__(self):
         """Returns a string representation of the instance"""
