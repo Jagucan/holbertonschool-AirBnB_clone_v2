d73a5b023fa266aa6a60334290457b5606298f94
Jagucan <jjgc.2499@gmail.com>
2023-03-23 18:29:13 -0500

    changes  on place, user and review

--- models/place.py
+++ models/place.py
@@ -6,34 +6,35 @@ from sqlalchemy import Column, String, Integer, Float, ForeignKey
 import os
 
 
-class Place(BaseModel, Base):
+class Place(BaseModel, Base if os.getenv('HBNB_TYPE_STORAGE') == 'db' else object):
     """ A place to stay """
-
-    __tablename__ = "places"
-
-    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=True)
-    number_rooms = Column(Integer, default=0, nullable=False)
-    number_bathrooms = Column(Integer, default=0, nullable=False)
-    max_guest = Column(Integer, default=0, nullable=False)
-    price_by_night = Column(Integer, default=0, nullable=False)
-    latitude = Column(Float, nullable=False)
-    longitude = Column(Float, nullable=False)
-    amenity_ids = []
-
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = "places"
+
+        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        description = Column(String(1024), nullable=True)
+        number_rooms = Column(Integer, default=0, nullable=False)
+        number_bathrooms = Column(Integer, default=0, nullable=False)
+        max_guest = Column(Integer, default=0, nullable=False)
+        price_by_night = Column(Integer, default=0, nullable=False)
+        latitude = Column(Float, nullable=False)
+        longitude = Column(Float, nullable=False)
+        amenity_ids = []
+
         reviews = relationship('Review', backref='place',
                                cascade='all, delete-orphan')
 
     else:
-        @property
-        def rev(self):
-            """Returns the list of Review instances"""
-            from models.review import Review
-            rev_list = []
-            for r in storage.all(Review).values():
-                if r.place_id == self.id:
-                    rev_list.append(r)
-            return rev_list
+        city_id = ""
+        user_id = ""
+        name = ""
+        description = ""
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0
+        latitude = 0.0
+        longitude = 0.0
+        amenity_ids = []
--- models/review.py
+++ models/review.py
@@ -7,8 +7,9 @@ import os
 
 class Review(BaseModel, Base if os.getenv('HBNB_TYPE_STORAGE') == 'db' else object):
     """ Review class to store review information """
-    __tablename__="reviews"
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = "reviews"
 
-    place_id=Column(String(60), ForeignKey("places.id"), nullable = False)
-    user_id=Column(String(60), ForeignKey("users.id"), nullable = False)
-    text=Column(String(1024), nullable = False)
+        place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+        text = Column(String(1024), nullable=False)
