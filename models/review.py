955b4adc401ab877998d7317b099a38d167d66d0
Jagucan <jjgc.2499@gmail.com>
2023-03-23 17:56:57 -0500

    update review

--- models/review.py
+++ models/review.py
@@ -4,10 +4,10 @@ from models.base_model import Base, BaseModel
 from sqlalchemy import Column, String, ForeignKey
 
 
-class Review(BaseModel, Base):
+class Review(BaseModel, Base if os.getenv('HBNB_TYPE_STORAGE') == 'db' else object)):
     """ Review class to store review information """
-    __tablename__ = "reviews"
+    __tablename__="reviews"
 
-    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
-    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
-    text = Column(String(1024), nullable=False)
+    place_id=Column(String(60), ForeignKey("places.id"), nullable = False)
+    user_id=Column(String(60), ForeignKey("users.id"), nullable = False)
+    text=Column(String(1024), nullable = False)
