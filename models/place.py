a6ff8a11cfbb9e7d07797a2c899eca1fc8d21569
Dannyelgeek <daot1993@gmail.com>
2023-03-24 14:35:47 -0500

    place AirBnB clone V2 update.

--- models/place.py
+++ models/place.py
@@ -15,7 +15,7 @@ class Place(BaseModel, Base if os.getenv('HBNB_TYPE_STORAGE') == 'db' else objec
         city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
         name = Column(String(128), nullable=False)
-        description = Column(String(1024), nullable=True)
+        description = Column(String(1024))
         number_rooms = Column(Integer, default=0, nullable=False)
         number_bathrooms = Column(Integer, default=0, nullable=False)
         max_guest = Column(Integer, default=0, nullable=False)
