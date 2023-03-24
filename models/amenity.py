36c44a00c97cd018e5a3cfdd221e11f125afefee
Dannyelgeek <daot1993@gmail.com>
2023-03-24 10:56:33 -0500

    amenity AirBnB clone V2 update.

--- models/amenity.py
+++ models/amenity.py
@@ -11,6 +11,4 @@ class Amenity(BaseModel, Base if os.getenv('HBNB_TYPE_STORAGE') == 'db' else obj
         __tablename__ = "amenities"
 
         name = Column(String(128), nullable=False)
-        place_amenities = relationship("")
-    else:
-        name = ""
+
