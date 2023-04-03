1a05b579652febaa652243bd0c409108f25ca346
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-04-03 13:22:55 -0500

    Hello Flask!

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+""" Starts a Flask web application """
 
 from flask import Flask
+
 
 app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
+    """ Handles the Route to the Flask application """
     return "Hello HBNB!"
 
 if __name__ == "__main__":
