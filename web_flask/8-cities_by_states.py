22f5f3f5b1e75aea72e84aeb7d9db0be0601615b
Jacinto Gutierrez <jjgc.2499@gmail.com>
2023-04-05 13:38:42 -0500

    Cities by states

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -26,7 +26,8 @@ def states():
                 cities.append(city)
 
     sorted_cities = sorted(cities, key=lambda city: city.name)
-    return render_template('8-cities_by_states.html', states=sorted_states, cities=sorted_cities)
+    return render_template('8-cities_by_states.html',
+                           states=sorted_states, cities=sorted_cities)
 
 
 @app.teardown_appcontext
